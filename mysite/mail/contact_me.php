2fdb070f342b8d55541d2b222c6be855be06db49
Matias Romero <matias6787@gmail.com>
2018-07-08 21:11:33 -0300

    Email + Final

--- mysite/mail/contact_me.php
+++ mysite/mail/contact_me.php
@@ -1,4 +1,7 @@
 <?php
+require("class.phpmailer.php");
+require("class.smtp.php");
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -9,18 +12,50 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-	
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-	
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "mail.matiasromero.com.ar";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "me@matiasromero.com.ar";  // Mi cuenta de correo
+$smtpClave = "Mati1987";  // Mi contraseña
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "me@matiasromero.com.ar";
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 587; 
+$mail->IsHTML(true); 
+$mail->CharSet = "utf-8";
+
+$mail->Host = $smtpHost; 
+$mail->Username = $smtpUsuario; 
+$mail->Password = $smtpClave;
+
+$mail->From = $email_address; // Email desde donde envío el correo.
+$mail->FromName = $name;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+$mail->AddReplyTo($email); // Esto es para que al recibir el correo y poner Responder, lo haga a la cuenta del visitante. 
+$mail->Subject = "Email desde sitio"; // Este es el titulo del email.
+$mensajeHtml = nl2br($message);
+$mail->Body = "Mensaje: {$mensajeHtml} <br /><br />Nombre:{$name}<br />Email de contacto:{$email_address}<br /> Tel: {$phone}"; // Texto del email en formato HTML
+$mail->AltBody = "{$message} \n\n Formulario de ejemplo By DonWeb"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+$result = $mail->Send(); 
+if($result){
+    return true;
+} else {
+    return false;
+}		
 ?>
