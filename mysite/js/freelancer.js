52f776de900dd255357f23bb0499e103c9117b03
Matias Romero <matias6787@gmail.com>
2018-07-09 21:12:01 -0300

    Quotes

--- mysite/js/freelancer.js
+++ mysite/js/freelancer.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +16,7 @@
   });
 
   // Scroll to top button appear
-  $(document).scroll(function() {
+  $(document).scroll(function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
@@ -26,7 +26,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -37,7 +37,7 @@
   });
 
   // Collapse Navbar
-  var navbarCollapse = function() {
+  var navbarCollapse = function () {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
@@ -56,18 +56,72 @@
     focus: '#username',
     modal: true
   });
-  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+  $(document).on('click', '.portfolio-modal-dismiss', function (e) {
     e.preventDefault();
     $.magnificPopup.close();
   });
 
+  let quotes = [{
+      author: 'ALAN KAY',
+      quote: 'The best way to predict the future is to implement it.'
+    },
+    {
+      author: 'CUNNINGHAMS LAW',
+      quote: 'The best way to get the right answer on the internet is not to ask a question;it \'s to post the wrong answer.'
+    },
+    {
+      author: 'WASEEM LATIF',
+      quote: 'Programming is not easy like Sunday morning, it is silent poetry.'
+    },
+    {
+      author: 'ROY CARLSON',
+      quote: 'The sooner you start to code, the longer the program will take.'
+    },
+    {
+      author: 'TIM KADLEC',
+      quote: 'Blame the implementation, not the technique.'
+    },
+    {
+      author: 'LOUIS SRYGLEY',
+      quote: 'Without requirements or design, programming is the art of adding bugs to an empty text file.'
+    },
+    {
+      author: 'JOHN JOHNSON',
+      quote: 'First, solve the problem. Then, write the code.'
+    },
+    {
+      author: 'JOHN O\'NOLAN ',
+      quote: 'It\'s a lot easier to talk about code than it is to write code.'
+    },
+    {
+      author: 'ADDY OSMANI',
+      quote: 'First do it, then do it right, then do it better.'
+    },
+    {
+      author: 'JOHN SHORE',
+      quote: 'Good programming is good writing.'
+    },
+    {
+      author: 'C. A. R. HOARE',
+      quote: 'The most important property of a program is whether it accomplishes the intention of its user.'
+    },
+    {
+      author: 'KEN THOMPSON',
+      quote: 'One of my most productive days was throwing away 1000 lines of code.'
+    }
+  ];
+
   // Floating label headings for the contact form
-  $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+  $(function () {
+    var quote = quotes[Math.floor(Math.random() * quotes.length)];
+    $('#quote').text(quote.quote);
+    $('#author').text('─ ' + quote.author);
+
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
       $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
+    }).on("focus", ".floating-label-form-group", function () {
       $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
+    }).on("blur", ".floating-label-form-group", function () {
       $(this).removeClass("floating-label-form-group-with-focus");
     });
   });
